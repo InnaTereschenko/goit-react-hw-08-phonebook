28bd5e15ca6733ed1b8501e98fbaa551ace65ff7
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-09-18 14:09:24 +0300

    add Contact Page

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,18 +1,19 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logOut } from "redux/auth/authOperations";
 import styles from './UserMenu.module.css';
 import defaultAvatar from './defaultAvatar.png';
-import { selectUser } from "redux/auth/authSelectors";
+import { useAuth } from "components/hooks";
 
 const UserMenu = () => {
-    const dispatch = useDispatch();
-    const name = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+    
  const avatar = defaultAvatar;
 
   return (
     <div className={styles.container}>
       <img src={avatar} alt="User" width="32" className={styles.avatar} />
-      <span className={styles.name}>Welcome, {name}</span>
+      <span className={styles.name}>Welcome, {user.name}</span>
       <button type="button" onClick={() => dispatch(logOut())}>
         Log Out
       </button>
---
+++ src/pages/ContactsPage/ContactsPage.js
@@ -0,0 +1,30 @@
+import { ContactForm } from '../../components/ContactForm/ContactForm.jsx';
+import { ContactList } from '../../components/ContactList/ContactList';
+import { Filter } from '../../components/Filter/Filter.jsx';
+import { useEffect } from 'react';
+import { selectContacts } from '../../redux/contacts/selectors.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operetions';
+
+export function App() {
+  const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1 className="mainTitle">Phonebook</h1>
+      <ContactForm />
+      {contacts.length > 0 && (
+        <>
+          <Filter />
+
+          <ContactList />
+        </>
+      )}
+    </div>
+  );
+}
